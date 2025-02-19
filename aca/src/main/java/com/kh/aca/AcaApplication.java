725cec2550456e3b3560ada8d8be7c90fb04db2f
hxxggu <hxxggu@gmail.com>
2025-02-19 09:51:58 +0900

    spring10ajax

---
+++ aca/src/main/java/com/kh/aca/AcaApplication.java
@@ -0,0 +1,13 @@
+package com.kh.aca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcaApplication.class, args);
+	}
+
+}
---
+++ aca/src/test/java/com/kh/aca/AcaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.aca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
